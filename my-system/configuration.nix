7e8c10f637ceae490dc2a666d366fda4704702c9
Stanislovas Kujalis <stanislovas@pikta.lt>
2024-12-18 23:44:33 +0200

    added screenshot hotkey

--- my-system/configuration.nix
+++ my-system/configuration.nix
@@ -13,6 +13,8 @@
   # Bootloader.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
